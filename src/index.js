189f90354eabd295e06c45c4c776e2e1d5e669f3
Sarah Shin <sarahsujin.shin@gmail.com>
2019-02-28 10:27:10 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,35 @@
 document.addEventListener('DOMContentLoaded', () => {
 
+  //VARIABLES===================================================================
+  const allDogsURL = "http://localhost:3000/dogs"
+  const dogTable = document.querySelector(".margin")
+  const tableHead = document.querySelector(".blue")
+  // const editButton = document.querySelector("#edit-button")
+
+  //CALLS=======================================================================
+  fetchDogs(allDogsURL)
+
+  //EVENT LISTENERS=============================================================
+  dogTable.addEventListener('click', (e)=> {
+    console.log(e.target)
+  })
+
+  //FUNCTIONS===================================================================
+  function fetchDogs(url){
+    fetch(url)
+    .then(response => response.json())
+    .then(parsedJSON => {
+      renderAllDogs(parsedJSON)
+    })
+  }
+
+  function renderDog(dog){
+    tableHead.innerHTML += `
+    <tr><td>${dog.name}</td> <td>${dog.breed}</td> <td>${dog.sex}</td> <td><button id="edit-button" data-id=${dog.id}>Edit</button></td></tr>
+    `
+  }
+
+  function renderAllDogs(dogs){
+    dogs.forEach(renderDog)
+  }
 })
